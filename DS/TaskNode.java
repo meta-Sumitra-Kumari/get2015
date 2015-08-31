665adfb94cd3ec8841e22e4dffe588d4d4a2ac78
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-08-31 17:48:53 +0530

    Create TaskNode.java

---
+++ DS/TaskNode.java
@@ -0,0 +1,19 @@
+public class TaskNode {
+
+	String job;
+	int priority;
+
+	/**
+	 * ===----Constructor calling
+	 * 
+	 * @param job
+	 *            ----job to be printed
+	 * @param priority
+	 *            -----its priority
+	 */
+	public TaskNode(String job, int priority) {
+		this.job = job;
+		this.priority = priority;
+	}
+
+}
