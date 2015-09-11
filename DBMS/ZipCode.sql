9ef6d5c1a0886bcea8eff5ebf0a8b5a2586f5fb8
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-09-11 17:27:18 +0530

    Update ZipCode.sql

--- DBMS/ZipCode.sql
+++ DBMS/ZipCode.sql
@@ -1,47 +1,40 @@
 CREATE DATABASE zipcodeInfo;
 USE zipcodeInfo;
 
--- creating table Zipcode
-CREATE TABLE zipcodeNumber(
-    zipcode int PRIMARY KEY AUTO_INCREMENT
+-- Creating table stateInfo
+CREATE TABLE stateInfo(
+    State_id INT PRIMARY KEY NOT NULL,
+    stateName varchar(20)
 );
 -- Creating table cityInfo
 CREATE TABLE cityInfo(
-    zipcode int PRIMARY KEY AUTO_INCREMENT,
+    zipcode INT PRIMARY KEY AUTO_INCREMENT,
+    State_id INT NOT NULL,
     cityName varchar(20),
-    FOREIGN KEY (zipcode)
-        REFERENCES zipcodeNumber (zipcode)
+    FOREIGN KEY (State_id)
+        REFERENCES stateInfo (State_id)
 );
--- Creating table stateInfo
-CREATE TABLE stateInfo(
-    zipcode int PRIMARY KEY,
-    stateName varchar(20),
-    FOREIGN KEY (zipcode)
-        REFERENCES zipcodeNumber (zipcode)
-);
- -- Inserting values in zipcodeNumber table
-INSERT INTO zipcodeNumber(zipcode) VALUES (801);
-INSERT INTO zipcodeNumber(zipcode) VALUES (802);
-INSERT INTO zipcodeNumber(zipcode) VALUES (803);
+
+ -- Inserting values in stateInfo table
+INSERT INTO stateInfo(State_id,stateName) VALUES (801,'bihar');
+INSERT INTO stateInfo(State_id,stateName) VALUES (802,'rajasthan');
+INSERT INTO stateInfo(State_id,stateName) VALUES (803,'delhi');
+
  
  -- Inserting values in cityInfo table
-INSERT INTO cityInfo(zipcode,cityName) VALUES (801,'patna');
-INSERT INTO cityInfo(cityName) VALUES ('jaipur');
-INSERT INTO cityInfo(cityName) VALUES ('delhi');
+INSERT INTO cityInfo(zipcode,cityName,State_id) VALUES (101,'patna',801);
+INSERT INTO cityInfo(zipcode,cityName,State_id) VALUES (102,'jaipur',802);
+INSERT INTO cityInfo(zipcode,cityName,State_id) VALUES (103,'delhi',803);
  
- -- Inserting values in stateInfo table
-INSERT INTO stateInfo(zipcode,stateName) VALUES (801,'bihar');
-INSERT INTO stateInfo(zipcode,stateName) VALUES (802,'rajasthan');
-INSERT INTO stateInfo(zipcode,stateName) VALUES (803,'delhi');
+
  -- Query to find the zip code city name and stateName
 SELECT 
-    z.zipcode, cityName, stateName
+    c.zipcode, cityName, stateName
 from
-    zipcodeNumber z,
     cityInfo c,
     stateInfo s
 where
-    ((z.zipcode = c.zipcode) && (z.zipcode = s.zipcode))
+(c.State_id = s.State_id)
 ORDER BY stateName asc ,cityName asc;
 
  
