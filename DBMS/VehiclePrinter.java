4287ccab3518c5db8360a53c7be5fea316a90f07
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-09-22 19:07:02 +0530

    Create VehiclePrinter.java

---
+++ DBMS/VehiclePrinter.java
@@ -0,0 +1,19 @@
+/**
+ * @author Sumitra Class to print objects
+ *
+ */
+public class VehiclePrinter {
+	/**
+	 * 
+	 * Function to print vehicles
+	 * 
+	 * @param vehicle
+	 * @return
+	 */
+	public static String printVehicleSpecification(Vehicle vehicle) {
+
+		return vehicle.toString();
+
+	}
+
+}
