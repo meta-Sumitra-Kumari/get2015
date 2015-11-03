0f7236e31975de0c6969940aceee70f67b580727
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-11-03 23:17:45 +0530

    Update Company.java

--- Web/DesignPattern/src/Assignment2/Company.java
+++ Web/DesignPattern/src/Assignment2/Company.java
@@ -1,1 +1,27 @@
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Sumitra this is comapny class to add the engineer in the list
+ */
+public class Company {
+	static List<Engineer> engineerList = new ArrayList<Engineer>();
+
+	/**
+	 * 
+	 * @param engineer
+	 *            -- object of engineer to add engineer in list
+	 */
+	public void add(Engineer engineer) {
+		engineerList.add(engineer);
+	}
+
+	/**
+	 * 
+	 * @return list of engineer
+	 */
+	public List<Engineer> getList() {
+		return engineerList;
+	}
+}
