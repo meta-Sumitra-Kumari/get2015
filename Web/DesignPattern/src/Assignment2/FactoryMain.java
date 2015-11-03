879d63d5f3f1d2590db5e43009e009283cd623e2
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-11-03 23:18:11 +0530

    Update FactoryMain.java

--- Web/DesignPattern/src/Assignment2/FactoryMain.java
+++ Web/DesignPattern/src/Assignment2/FactoryMain.java
@@ -1,28 +1,28 @@
-package Assignment2;
-
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 /**
- * @author Sumitra --Factory class which will decide to whom's instance is to
- *         create and perform all other functions
- *
+ * @author Sumitra this is the factory class to implement the factory method
  */
-public class FactoryMain {
+public class Factory {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
 		String name;
 		String role;
 		char choice;
+		Company company = new Company();
 		// getting instance of company
-		
-		Scanner scanner = new Scanner(System.in);
+
 		do {
-			System.out.println("1 ->To add Frontend Engineer");
+			System.out.println("1 ->To add Engineer");
 			System.out.println("2 ->to print list of Front end Engineer");
 
-			System.out.println("3 ->To add backend Engineer");
-			System.out.println("4 ->to print list of back End Engineer"); 
+			System.out.println("3 ->to print list of Back end Engineer");
 
 			int doChoice;
 			do {// Validation
@@ -35,35 +35,75 @@ public class FactoryMain {
 				}
 				doChoice = scanner.nextInt();
 			} while (doChoice < 0);
+			Engineer engineer = null;
 			switch (doChoice) {
 			case 1:
-				// adding data in list
-				System.out.println("Enter name---");
+				System.out.println("enter name");
 				name = scanner.next();
-				FrontHandEngineer frontengineer = FrontHandEngineer.getInstance();
-				frontengineer.addFrontEndEngineer(name);
+				System.out.println("enter role of engineer");
+				role = scanner.next();
+				// according to role object of engineer is instantiated.
+				if (role.equalsIgnoreCase(Role.FrontEndEngineer.toString())) {
+					// object of front end engineer is created
+					engineer = new FrontEndEngineer();
+				} else if (role.equalsIgnoreCase(Role.BackEndEngineer
+						.toString())) {
+					// object of back end engineer is created
+					engineer = new BackEndEngineer();
+				} else {
+					// otherwise no object is created
+					System.out.println("enter valid role for engineer");
+					break;
+				}
+				engineer.setName(name);
+				engineer.setRole();
+				company.add(engineer);
 				break;
 			case 2:
-				// showing list
-				FrontHandEngineer engineerList = FrontHandEngineer.getInstance();
-				engineerList.showList();
+				// to print the list of front end engineer
+				List<Engineer> engineerList = company.getList();
+				Iterator<Engineer> itr = engineerList.iterator();
+				int i = 1;
+				while (itr.hasNext()) {
+					Engineer engineer2 = itr.next();
+					if (engineer2.getRole().equalsIgnoreCase(
+							Role.FrontEndEngineer.toString())) {
+						System.out.println("Id ->" + i++);
+						System.out.println("Name ->" + engineer2.getName());
+						System.out.println("Role ->" + engineer2.getRole());
+						System.out.println("--------------");
+					}
+				}
+				// if no front end engineer is available
+				if (i == 1) {
+					System.out.println("No FrontEndEngineer Available");
+					System.out.println("----------");
+				}
 				break;
 			case 3:
-				System.out.println("Enter name---");
-				name = scanner.next();
-				BackHandEngineer backengineer = BackHandEngineer.getInstance();
-				backengineer.addBackEndEngineer(name);
+				// to print the list of back end engineer
+				List<Engineer> engineerList1 = company.getList();
+				Iterator<Engineer> itr1 = engineerList1.iterator();
+				int id = 1;
+				while (itr1.hasNext()) {
+					Engineer engineer2 = itr1.next();
+					if (engineer2.getRole().equalsIgnoreCase(
+							Role.BackEndEngineer.toString())) {
+						System.out.println("ID ->" + id++);
+						System.out.println("Name ->" + engineer2.getName());
+						System.out.println("Role ->" + engineer2.getRole());
+						System.out.println("-------------");
+					}
+				}
+				// if no back end engineer is available
+				if (id == 1) {
+					System.out.println("No BackEndEngineer Available");
+					System.out.println("--------------");
+				}
 				break;
-			case 4:
-				BackHandEngineer backEngineerList = BackHandEngineer.getInstance();
-				backEngineerList.showList();
-				break;
-				
 			default:
-				System.out.println("enter correct choice");
-				break;
+				System.out.println("Entered wrong chocie");
 			}
-
 			System.out.println("want to continue(y/n)");
 			choice = scanner.next().charAt(0);
 		} while (choice == 'y' || choice == 'Y');
