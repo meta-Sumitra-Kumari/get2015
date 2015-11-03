41791990422678b30bb7e4f1c53ab1f2063f512f
meta-Sumitra-Kumari <meta-Sumitra-Kumari@users.noreply.github.com>
2015-11-03 23:16:39 +0530

    Update Engineer.java

--- Web/DesignPattern/src/Assignment2/Engineer.java
+++ Web/DesignPattern/src/Assignment2/Engineer.java
@@ -1,3 +1,7 @@
+/**
+ * @author Sumitra --this is interface for engineer having method to set and get
+ *         name and role of engineer
+ */
 public interface Engineer {
 	void setName(String name);
 
